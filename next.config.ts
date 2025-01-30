51b9d44b24daab11148f7c85015e1d5e42bef13e
Gustavo <guxgomes@gmail.com>
2025-01-30 02:10:48 -0300

    Commitado Atualizado2

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ["https://b748-2804-1b2-11c0-2b7b-f835-57a-42e-ca3c.ngrok-free.app"],
+    domains: ["https://9686-2804-1b2-11c0-2b7b-f835-57a-42e-ca3c.ngrok-free.app"],
   },
 };
 
